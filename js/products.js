244fbe44ea43c489e43cd68704e2a05d457eba46
homiesbaq <gabotraslee@gmail.com>
2026-02-01 13:37:35 -0500

    Create products.js

---
+++ js/products.js
@@ -0,0 +1,20 @@
+const products = [
+  {
+    id: "homies-tee",
+    name: "Homies Tee",
+    price: "$120.000 COP",
+    images: ["img/h1.jpg","img/h2.jpg"]
+  },
+  {
+    id: "street-cap",
+    name: "Street Cap",
+    price: "$90.000 COP",
+    images: ["img/cap.jpg"]
+  },
+  {
+    id: "tote-black",
+    name: "Tote Bag",
+    price: "$70.000 COP",
+    images: ["img/tote.jpg"]
+  }
+];
