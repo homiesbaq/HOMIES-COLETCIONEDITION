7261d747b7fb2e8525d199d6fa93cdee9d0d32fc
homiesbaq <gabotraslee@gmail.com>
2026-02-01 14:38:36 -0500

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,20 +1,91 @@
 const products = [
   {
-    id: "homies-tee",
-    name: "Homies Tee",
-    price: "$120.000 COP",
-    images: ["img/h1.jpg","img/h2.jpg"]
+    id: 1,
+    name: "Camiseta Rey Momo",
+    price: 90000,
+    image: "img/producto1.jpg",
+    colors: ["Rojo", "Negro"],
+    sizes: ["S", "M", "L"]
   },
   {
-    id: "street-cap",
-    name: "Street Cap",
-    price: "$90.000 COP",
-    images: ["img/cap.jpg"]
+    id: 2,
+    name: "Hoodie Carnaval",
+    price: 160000,
+    image: "img/producto2.jpg",
+    colors: ["Negro"],
+    sizes: ["M", "L", "XL"]
   },
   {
-    id: "tote-black",
-    name: "Tote Bag",
-    price: "$70.000 COP",
-    images: ["img/tote.jpg"]
-  }
+    id: 2,
+    name: "Hoodie Carnaval",
+    price: 160000,
+    image: "img/producto2.jpg",
+    colors: ["Negro"],
+    sizes: ["M", "L", "XL"]
+  },
+  {
+    id: 3,
+    name: "Hoodie cervecero",
+    price: 160000,
+    image: "img/producto2.jpg",
+    colors: ["Negro"],
+    sizes: ["M", "L", "XL"]
+  },
+  {
+    id: 4,
+    name: "Hoodie mariamoñitos",
+    price: 160000,
+    image: "img/producto2.jpg",
+    colors: ["Negro"],
+    sizes: ["M", "L", "XL"]
+  },
+  {
+    id: 5,
+    name: "Hoodie mood caribeño",
+    price: 160000,
+    image: "img/producto2.jpg",
+    colors: ["Negro"],
+    sizes: ["M", "L", "XL"]
+  },
+  {
+    id: 6,
+    name: "Hoodie picotero",
+    price: 160000,
+    image: "img/producto2.jpg",
+    colors: ["Negro"],
+    sizes: ["M", "L", "XL"]
+  },
+  {
+    id: 7,
+    name: "Hoodie esquina bacana",
+    price: 160000,
+    image: "img/producto2.jpg",
+    colors: ["Negro"],
+    sizes: ["M", "L", "XL"]
+  },
+  {
+    id: 8,
+    name: "Hoodie blush",
+    price: 160000,
+    image: "img/producto2.jpg",
+    colors: ["Negro"],
+    sizes: ["M", "L", "XL"]
+  },
+  {
+    id: 9,
+    name: "Hoodie costeñita core",
+    price: 160000,
+    image: "img/producto2.jpg",
+    colors: ["Negro"],
+    sizes: ["M", "L", "XL"]
+  },
+  {
+    id: 10,
+    name: "Hoodie caiman",
+    price: 160000,
+    image: "img/producto2.jpg",
+    colors: ["Negro"],
+    sizes: ["M", "L", "XL"]
+  },
+  // copia y cambia hasta tener 10
 ];
